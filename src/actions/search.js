d68b723f20324a116df4938795f1ce4ef426012b
Zack Miller <hotbodyfitness@gmail.com>
2019-06-05 16:20:46 -0700

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,19 +3,16 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
 var handleVideoSearch = (q) => {
-  //TODO:  Write an asynchronous action to handle a video search!
+
   return (dispatch) => {
-    searchYouTube({key: YOUTUBE_API_KEY,
-      query: q}, changeVideoList);
+    searchYouTube({ 'key': YOUTUBE_API_KEY, 'query': q },
+      (videos) => {
+        dispatch(changeVideoList(videos));
+        dispatch(changeVideo(videos[0]));
+      });
+
   };
-
-
-  // return {
-  //   type: 'HANDLE_VIDEO_SEARCH',
-  //   q: searchYouTube(q)
-  // }
 };
 
 export default handleVideoSearch;
