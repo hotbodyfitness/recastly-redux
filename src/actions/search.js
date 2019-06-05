256bcaa995b6223de1e1d0e3c05d366b9b705ecb
Zack Miller <hotbodyfitness@gmail.com>
2019-06-04 20:52:46 -0700

    Working on Actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,17 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY,
+      query: q}, changeVideoList);
+  };
+
+
+  // return {
+  //   type: 'HANDLE_VIDEO_SEARCH',
+  //   q: searchYouTube(q)
+  // }
 };
 
 export default handleVideoSearch;
