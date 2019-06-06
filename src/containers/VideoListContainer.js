111f1606efdaa3fec6cdd007654f59db910c528a
Zack Miller <hotbodyfitness@gmail.com>
2019-06-05 18:01:40 -0700

    working on Containers

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -1,8 +1,18 @@
 import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
-import changeVideo from './../actions/currentVideo.js';
+// import changeVideo from './../actions/currentVideo.js';
+import changeVideoList from './../actions/videoList.js';
 
-var VideoListContainer = () => {};
+
+var VideoListContainer = () => {
+  // figure out videos that we need to pass to videoList presentation component
+  // define fn that filters state.videos
+  var mapStateToProps = (state) => (
+    {
+      videos: unknownFunction(state.videos, state.changeVideo)
+    }
+  );
+};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
